d42439acf94b74537a5544122af9834010cb8e73
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2025-11-22 20:08:46 -0300

    fix: port value

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ import "dotenv/config"
 import app from "./src/app.js";
 
 
-const PORT = 3000 
+const port = process.env.PORT || 3000
 
-app.listen(PORT,()=>{
+app.listen(port,()=>{
     console.log('server listening')
 });
 
