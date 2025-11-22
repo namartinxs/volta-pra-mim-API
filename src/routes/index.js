687d98526a7d38b588d5c72271408ddd4b981725
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2025-11-22 13:40:11 -0300

    feat: add claim route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,11 @@ import express from 'express'
 import LostItems from './lostItemsRoutes.js'
 import User from './userRoutes.js'
 import Spot from './SpotRoutes.js'
+import Claim from './claimRoutes.js'
 
 const routes = (app)=>{
     app.route('/').get((req,res)=>res.status(200).send('api achados e perdidos'))
-    app.use(express.json(),LostItems,User,Spot);
+    app.use(express.json(),LostItems,User,Spot,Claim);
 
     
 };
