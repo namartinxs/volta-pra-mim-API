a851e29a020685b53f418ceff4c5fe47289c7562
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2025-11-22 12:34:50 -0300

    feat: add spot route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import LostItems from './lostItemsRoutes.js'
 import User from './userRoutes.js'
+import Spot from './SpotRoutes.js'
 
 const routes = (app)=>{
     app.route('/').get((req,res)=>res.status(200).send('api achados e perdidos'))
-    app.use(express.json(),LostItems,User);
+    app.use(express.json(),LostItems,User,Spot);
 
     
 };
