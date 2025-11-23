3e445ea31f29d92c191b6ae7354411922c01c33f
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2025-11-23 15:43:32 -0300

    fix: fix parameter

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,6 @@ import {login,logout,authValidate} from "../controllers/authController.js"
 const routes = Router()
 routes.post('/login',login)
 routes.post('/logout',logout)
-routes.post('/validate',authValidate)
+routes.get('/validate',authValidate)
 
 export default routes
