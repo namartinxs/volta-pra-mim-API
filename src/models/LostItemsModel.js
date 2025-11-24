3a53ed003179b4233901172e23c7ba2c4d00ad52
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2025-11-24 19:41:58 -0300

    fix: emergencial2

--- src/models/LostItemsModel.js
+++ src/models/LostItemsModel.js
@@ -2,30 +2,18 @@ import mongoose from "mongoose";
 // id: {type: mongoose.Schema.Types.ObjectId,required:true},
 //OBJETO DE CONFIG as propiedades dos objetos
 const LostSchema = new mongoose.Schema({
-    itemName:{type:mongoose.Schema.Types.String,required:true},
-    fileName:{ type: mongoose.Schema.Types.String,required:true},
+    itemName:{type:mongoose.Schema.Types.String},
+    fileName:{ type: mongoose.Schema.Types.String},
     statusItem:{type:mongoose.Schema.Types.String,
         enum: ["aguardando_reclamacao", "reclamado", "devolvido"],
         default: "aguardando_reclamacao"
     },
     description:{type:mongoose.Schema.Types.String},
-    locationFound:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'spot'
-    },
-    dateFound:{type:mongoose.Schema.Types.Date,required:true},
-    foundBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
-    },
-    receivedBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
-    },
-    collectedBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
-    },
+    locationFound:{type:mongoose.Schema.Types.String},
+    dateFound:{type:mongoose.Schema.Types.Date},
+    foundBy: {type:mongoose.Schema.Types.String},
+    receivedBy: {type:mongoose.Schema.Types.String},
+    collectedBy: {type:mongoose.Schema.Types.String},
     dateColleted:{type:mongoose.Schema.Types.Date}
 
 },{versionKey:false,timestamps:true})
